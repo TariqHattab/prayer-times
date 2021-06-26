af2ac35b844e36538855bd9773a9700818938fbf
TariqHattab <trqzh123@gmail.com>
2021-06-26 19:33:34 +0300

    commit 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
@@ -31,6 +32,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _times = {};
   Future<void> _getData() async {
     var parameters = {
       'city': 'Jeddah',
@@ -45,31 +47,115 @@ class _MyHomePageState extends State<MyHomePage> {
     var response = await http.get(url);
     print('Response status: ${response.statusCode}');
     // print('Response body: ${response.body}');
-    var timings = jsonDecode(response.body)['data']['timings'] as Map;
-    print('Response body: ${timings}');
-    print(timings.values);
+    _times = jsonDecode(response.body)['data']['timings'] as Map;
+    print('Response body: ${_times}');
+    setState(() {});
+    print(_times.values);
+  }
+
+  Widget buildCellContainer({
+    Widget child,
+    double padding,
+  }) {
+    return Container(
+        padding: EdgeInsets.all(padding),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.green[200],
+              Colors.green[300],
+            ],
+          ),
+          border: Border.all(
+            width: 2,
+            color: Colors.green,
+          ),
+        ),
+        child: child);
   }
 
   @override
   Widget build(BuildContext context) {
+    var keys = _times.keys.toList();
+    var values = _times.values.toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        // flexibleSpace: Container(
+        //   decoration: BoxDecoration(
+        //     gradient: LinearGradient(
+        //       begin: Alignment.centerRight,
+        //       end: Alignment.centerLeft,
+        //       colors: [
+        //         Colors.green[200],
+        //         Colors.green[300],
+        //       ],
+        //     ),
+        //   ),
+        // ),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          child: Row(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: keys
+                  .map(
+                    (e) => buildCellContainer(
+                        padding: 16,
+                        child:
+                            FittedBox(fit: BoxFit.scaleDown, child: Text(e))),
+                  )
+                  .toList(),
             ),
-            Text(
-              'fghfdh',
-              style: Theme.of(context).textTheme.headline4,
+          ),
+          Flexible(
+            flex: 4,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: values
+                  .map(
+                    (e) => buildCellContainer(
+                        padding: 16,
+                        child: Text(
+                          e,
+                          textAlign: TextAlign.center,
+                        )),
+                  )
+                  .toList(),
             ),
-          ],
-        ),
-      ),
+          ),
+        ],
+      )
+          // GridView.builder(
+          //   gridDelegate:
+          //       SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          //   itemBuilder: (ctx, index) {
+          //     var keys = _times.keys.toList();
+          //     var values = _times.values.toList();
+          //     return buildCellContainer(
+          //         padding: 0,
+          //         child: Row(
+          //           mainAxisAlignment: MainAxisAlignment.start,
+          //           children: [
+          //             buildCellContainer(
+          //               child: Text(keys[index]),
+          //               padding: 0,
+          //             ),
+          //             Expanded(
+          //               child: buildCellContainer(
+          //                 child: Text(values[index]),
+          //                 padding: 0,
+          //               ),
+          //             )
+          //           ],
+          //         ));
+          //   },
+          //   itemCount: _times.length,
+          // ),
+          ),
       floatingActionButton: FloatingActionButton(
         onPressed: _getData,
         tooltip: 'getData',
